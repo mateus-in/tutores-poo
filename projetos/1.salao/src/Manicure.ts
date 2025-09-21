8d8cba68e50c80274aad679150d742612fdcad89
WendelBrasil08 <wendelgabriel813@gmail.com>
2025-09-21 11:35:08 -0300

    Metodos de Classe promocao melhorados e Implementacao de desconto automatico nas classes Corte e Manicure, implementar tambem na classe Hidratacao

--- projetos/1.salao/src/Manicure.ts
+++ projetos/1.salao/src/Manicure.ts
@@ -1,16 +1,23 @@
 import { Servico } from './Servico';
-
+import { Promocao } from './Promocao';
 export class Manicure implements Servico {
   constructor(
+    public promocao: Promocao,
     public nome: string,
     public precoBase: number,
     public duracaoMinutos: number,
     public tipoProduto: string,
-  ) {
-    //
-  }
+  ) {}
 
   calcularPrecoFinal(): number {
-    return this.precoBase;
+    const estaAtiva = this.promocao.estaAtiva();
+    const aplicavelAoServico = this.promocao.aplicavelAoServico(this);
+    const percentualDesconto = this.promocao.percentualDesconto;
+    const desconto = this.promocao.calcularDesconto(this.precoBase);
+
+    if (!estaAtiva && !aplicavelAoServico) {
+      return this.precoBase;
+    }
+    return this.precoBase - desconto;
   }
 }
