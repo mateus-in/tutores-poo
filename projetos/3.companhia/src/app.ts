14e3c349b0874d28c39bf3591e693b36e4751141
isa422 <isadorasilvarosa01@gmail.com>
2025-10-03 12:29:41 -0300

    implementacao passageiro crianca

--- projetos/3.companhia/src/app.ts
+++ projetos/3.companhia/src/app.ts
@@ -1,3 +1,12 @@
 console.log('3.companhia');
 
 // teste 
+import { Aeronave
+ } from "./Aeronave";
+ import { StatusAeronave } from "./enums/StatusAeronave";
+const aeronave = new Aeronave("PT-123", "Boeing 737", 180, StatusAeronave.DISPONIVEL);
+
+console.log(aeronave.estarDisponivel()); // true
+
+aeronave.alterarStatus(StatusAeronave.MANUTENCAO);
+console.log(aeronave.estarDisponivel()); // false
