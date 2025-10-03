d4921097a0096d833a6cebd8b0e942087dbb4ca7
isa422 <isadorasilvarosa01@gmail.com>
2025-10-03 12:41:25 -0300

    implementacao classe vip

--- projetos/3.companhia/src/PassageiroVip.ts
+++ projetos/3.companhia/src/PassageiroVip.ts
@@ -1,5 +1,6 @@
 import { Bagagem } from './Bagagem';
 import { Passageiro } from './Passageiro';
+import { TipoBagagem } from './enums/TipoBagagem';
 
 export class PassageiroVip extends Passageiro {
   constructor(
@@ -18,9 +19,25 @@ export class PassageiroVip extends Passageiro {
   }
 
   calcularPrecoPassagem(precoBase: number): number {
-    return 1;
+        return precoBase * 0.8;
   }
   validarBagagem(bagagens: Bagagem[]): boolean {
-    return true;
+     const contarBagagens = (tipo: TipoBagagem) => 
+                    bagagens.filter(b => b.tipo === tipo).length;
+                  
+                const contagemMao = contarBagagens(TipoBagagem.BAGAGEM_MAO);
+                const contagemDespachada = contarBagagens(TipoBagagem.BAGAGEM_DESPACHADA);
+                const contagemEspecial = contarBagagens(TipoBagagem.BAGAGEM_ESPECIAL);
+        
+                      if (contagemMao> 1 ) {
+                        return false; 
+                      } else if (contagemDespachada > 2) {
+                        return false;
+                      }else if (contagemEspecial > 1){
+                        return  false ;
+                      } else {
+                        return true;
+                      }
+      }
   }
-}
+
