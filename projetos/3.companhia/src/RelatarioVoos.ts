99be8f1976c32d4e69add34e53c7fb8df47989b3
isa422 <isadorasilvarosa01@gmail.com>
2025-10-03 14:10:59 -0300

    IMLEMENTACAO CLASSE RELATORIO VOO

--- projetos/3.companhia/src/RelatarioVoos.ts
+++ projetos/3.companhia/src/RelatarioVoos.ts
@@ -1,15 +1,36 @@
+import { StatusAssento } from './enums/StatusAssento';
 import { Voo } from './Voo';
+
 export class RelatarioVoos {
   constructor(public voos: Voo[]) {}
 
   calcularReceitaTotal(): number {
-    return 1;
+  
+      let receitaTotal = 0;
+        for (const voo of this.voos) {
+            for (const reserva of voo.reservas) {
+                receitaTotal += reserva.precoTotal; 
+            }
+        }
+        return receitaTotal;
   }
 
   calcularTaxaOcupacao(): number {
-    return 1;
-  }
+      let totalAssentos = 0;
+        let totalReservados = 0;
+
+        for (const voo of this.voos) {
+            totalAssentos += voo.assentos.size; // assumindo que assentos é um Map
+            for (const status of voo.assentos.values()) {
+                if (status === StatusAssento.RESERVADO) {
+                    totalReservados++;
+                }
+              }
+            }
+             return totalAssentos > 0 ? (totalReservados / totalAssentos) * 100 : 0;
+          }
   listarVoosMaisPopulares(): Voo[] {
-    return [];
+     const voosOrdenados = [...this.voos].sort((a, b) => b.reservas.length - a.reservas.length);
+        return voosOrdenados;
   }
 }
